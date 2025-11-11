d914340aa2e06d9003e00a0e8e1e6320bfab1f38
Vishal Puri <89836637+iamvishalpuri@users.noreply.github.com>
2025-11-11 21:46:44 +0530

    Refactor prime number function and input handling

--- 1. Write a C++ function to find a prime number..cpp
+++ 1. Write a C++ function to find a prime number..cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 using namespace std;
-bool isPrime(int num) {
-    if (num < 2) 
-        return false;
-    for (int i = 2; i * i <= num; i++)
-        if (num % i == 0) return false;
-    return true;
-}
+
+bool isPrime(int number);
 
 int main() {
-    int n;
-    cout << "Enter number: "; cin >> n;
-    if (isPrime(n))
-        cout << n << " is Prime";
+    int num;
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Please enter a non-negative number.\n";
+        return 1;
+    }
+    
+    if (isPrime(num))
+        cout << num << " is a Prime number.\n";
     else
-        cout << n << " is Not Prime";
+        cout << num << " is Not a Prime number.\n";
+
     return 0;
 }
 
+
+bool isPrime(int number) {
+    if (number < 2) return false;
+    for (int i = 2; i * i <= number; ++i) {
+        if (number % i == 0) return false;
+    }
+    return true;
+}
+
 Output
-Enter number : 17
-17 is Prime
+Enter a positive integer: 17
+17 is a Prime number.
