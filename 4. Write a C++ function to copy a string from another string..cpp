dc6ebe60d142b5fde45b955d264a662c2f08c322
Vishal Puri <89836637+iamvishalpuri@users.noreply.github.com>
2025-11-11 21:51:23 +0530

    Refactor string copy function to include max size

--- 4. Write a C++ function to copy a string from another string..cpp
+++ 4. Write a C++ function to copy a string from another string..cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-
-void copyString(char dest[], const char src[]) {
-    int i = 0;
-    while (src[i] != '\0') {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-}
-
+void copyString(char destination[], const char source[], int maxSize);
 int main() {
     char source[100], destination[100];
-    cout << "Enter string: ";
+    cout << "Enter a string: ";
     cin.getline(source, 100);
-    copyString(destination, source);
-    cout << "Copied: " << destination << endl;
+    
+    copyString(destination, source, 100);
+    cout << "Copied String: " << destination << endl;
     return 0;
 }
 
+void copyString(char destination[], const char source[], int maxSize) {
+    int i = 0;
+    while (source[i] != '\0' && i < maxSize - 1) {
+        destination[i] = source[i];
+        ++i;
+    }
+    destination[i] = '\0';
+}
+
 Output
-Enter string : Hello C++
-Copied : Hello C++
+Enter a string: Hello World
+Copied String: Hello World
