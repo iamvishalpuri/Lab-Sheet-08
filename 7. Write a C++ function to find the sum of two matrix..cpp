de8df88e7e96c0d258d04f20743ddfbd3c8799e1
Vishal Puri <89836637+iamvishalpuri@users.noreply.github.com>
2025-11-11 21:57:20 +0530

    Refactor matrix addition function and main logic

--- 7. Write a C++ function to find the sum of two matrix..cpp
+++ 7. Write a C++ function to find the sum of two matrix..cpp
@@ -1,44 +1,44 @@
 #include <iostream>
 using namespace std;
-
-void matrixSum(int a[][10], int b[][10], int result[][10], int rows, int cols) {
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            result[i][j] = a[i][j] + b[i][j];
-}
-
+const int MAX = 10;
+void addMatrices(int mat1[][MAX], int mat2[][MAX], int result[][MAX], int rows, int cols);
 int main() {
-    int r, c, m1[10][10], m2[10][10], sum[10][10];
-    cout << "Enter rows and cols: ";
-    cin >> r >> c;
-    cout << "Enter matrix 1:\n";
-    for (int i = 0; i < r; i++){
-    for (int j = 0; j < c; j++)
-        cin >> m1[i][j];
-}
-    cout << "Enter matrix 2:\n";
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++)
-            cin >> m2[i][j];
-    }
-    matrixSum(m1, m2, sum, r, c);
-    cout << "Sum:\n";
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) 
-            cout << sum[i][j] << " ";
+    int rows, cols;
+    cout << "Enter rows and columns: ";
+    cin >> rows >> cols;
+    int matrixA[MAX][MAX], matrixB[MAX][MAX], sumMatrix[MAX][MAX];
+    cout << "Enter Matrix A:\n";
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < cols; ++j) cin >> matrixA[i][j];
+    cout << "Enter Matrix B:\n";
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < cols; ++j) cin >> matrixB[i][j];
+
+    addMatrices(matrixA, matrixB, sumMatrix, rows, cols);
+    cout << "Sum Matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << sumMatrix[i][j] << " ";
+        }
         cout << endl;
     }
     return 0;
 }
 
+void addMatrices(int mat1[][MAX], int mat2[][MAX], int result[][MAX], int rows, int cols) {
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < cols; ++j)
+            result[i][j] = mat1[i][j] + mat2[i][j];
+}
+
 Output
-Enter rows and cols: 2 2
-Enter matrix 1 :
+Enter rows and columns: 2 2
+Enter Matrix A:
 1 2
 3 4
-Enter matrix 2 :
+Enter Matrix B:
 5 6
 7 8
-Sum :
-6 8
-10 12
+Sum Matrix:
+6 8 
+10 12 
