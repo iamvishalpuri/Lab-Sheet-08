69cf0a8cff1bd5bff309374b0f35fa0181851e9b
Vishal Puri <89836637+iamvishalpuri@users.noreply.github.com>
2025-11-11 21:54:40 +0530

    Refactor binary search implementation and input handling

--- 6. Write a C++ function to search an element using a binary search technique..cpp
+++ 6. Write a C++ function to search an element using a binary search technique..cpp
@@ -1,40 +1,39 @@
 #include <iostream>
 using namespace std;
+int binarySearch(int arr[], int size, int target);
+int main() {
+    int size, key;
+    cout << "Enter sorted array size: ";
+    cin >> size;
 
-int binarySearch(int arr[], int size, int key) {
+    int sortedArray[size];
+    cout << "Enter " << size << " sorted elements:\n";
+    for (int i = 0; i < size; ++i) cin >> sortedArray[i];
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int result = binarySearch(sortedArray, size, key);
+    if (result != -1)
+        cout << key << " found at index " << result << endl;
+    else
+        cout << key << " not found.\n";
+    return 0;
+}
+
+int binarySearch(int arr[], int size, int target) {
     int left = 0, right = size - 1;
     while (left <= right) {
-        int mid = (left + right) / 2;
-        if (arr[mid] == key)
-            return mid;
-        else if (arr[mid] < key) 
-            left = mid + 1;
-        else 
-            right = mid - 1;
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) return mid;
+        if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
     }
     return -1;
 }
 
-int main() {
-    int n, arr[100], key;
-    cout << "Enter size: "; 
-    cin >> n;
-    cout << "Enter sorted array:\n";
-    for (int i = 0; i < n; i++) 
-        cin >> arr[i];
-    cout << "Search: "; 
-    cin >> key;
-    int pos = binarySearch(arr, n, key);
-    if (pos != -1) 
-        cout << "Found at index " << pos << endl;
-    else 
-        cout << "Not found\n";
-    return 0;
-}
-
 Output
-Enter size : 5
-Enter sorted array :
-10 20 30 40 50
-Search : 30
-Found at index 2
+Enter sorted array size: 6
+Enter 6 sorted elements:
+10 20 30 40 50 60
+Enter element to search: 40
+40 found at index 3
