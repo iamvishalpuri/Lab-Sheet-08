1d524098d3e10830fd525040f220dc2f6843627b
Vishal Puri <89836637+iamvishalpuri@users.noreply.github.com>
2025-11-11 21:48:27 +0530

    Refactor Fibonacci function and improve output

--- 2. Write a C++ function to print the Fibonacci series..cpp
+++ 2. Write a C++ function to print the Fibonacci series..cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 using namespace std;
+void printFibonacci(int terms);
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+    if (n <= 0) {
+        cout << "Please enter a positive number.\n";
+        return 1;
+    }
+    printFibonacci(n);
+    return 0;
+}
+void printFibonacci(int terms) {
+    long long first = 0, second = 1, next;
+    cout << "Fibonacci Series: ";
 
-void Fibonacci(int terms) {
-    int a = 0, b = 1, c;
-    cout << "Fibonacci: ";
-    for (int i = 0; i < terms; i++) {
-        cout << a << " ";
-        c = a + b;
-        a = b; 
-        b = c;
+    if (terms >= 1) cout << first;
+    if (terms >= 2) cout << ", " << second;
+
+    for (int i = 3; i <= terms; ++i) {
+        next = first + second;
+        cout << ", " << next;
+        first = second;
+        second = next;
     }
     cout << endl;
 }
-
-int main() {
-    int n;
-    cout << "Enter terms: ";
-    cin >> n;
-    Fibonacci(n);
-    return 0;
-}
-
 Output
-Enter terms : 8
-Fibonacci : 0 1 1 2 3 5 8 13
+Enter number of terms: 8
+Fibonacci Series: 0, 1, 1, 2, 3, 5, 8, 13
