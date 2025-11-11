4130afee26cd2ef07e2e3cb1ad9e8a1e1bb69344
Vishal Puri <89836637+iamvishalpuri@users.noreply.github.com>
2025-11-11 21:50:13 +0530

    Refactor bubble sort implementation and main function

--- 3. Write a C++ function to apply the bubble shot technique on an array..cpp
+++ 3. Write a C++ function to apply the bubble shot technique on an array..cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 using namespace std;
+void bubbleSort(int arr[], int size);
+int main() {
+    int size;
+    cout << "Enter array size: ";
+    cin >> size;
+
+    int numbers[size];
+    cout << "Enter " << size << " elements:\n";
+    for (int i = 0; i < size; ++i) cin >> numbers[i];
+    
+    bubbleSort(numbers, size);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < size; ++i) cout << numbers[i] << " ";
+    cout << endl;
+    return 0;
+}
 
 void bubbleSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++)
-        for (int j = 0; j < size - 1 - i; j++)
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
-}
-
-int main() {
-    int n, a[100];
-    cout << "Enter size: "; 
-    cin >> n;
-    cout << "Enter elements:\n";
-    for (int i = 0; i < n; i++) 
-        cin >> a[i];
-    bubbleSort(a, n);
-    cout << "Sorted: ";
-    for (int i = 0; i < n; i++) 
-        cout << a[i] << " ";
-    cout << endl;
-    return 0;
+        }
+    }
 }
 
 Output
-Enter size : 5
-Enter elements :
+Enter array size: 5
+Enter 5 elements:
 64 34 25 12 22
-Sorted : 12 22 25 34 64
+Sorted Array: 12 22 25 34 64 
